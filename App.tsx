1adcb27c8fb0bd972ec818919d5659c4b2cf76fa
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2020-11-11 18:49:02 +0900

    Fetch blog from DynamoDB

--- App.tsx
+++ App.tsx
@@ -1,15 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
-import Amplify from 'aws-amplify'
+import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
+import { listBlogs } from './graphql/queries'
 
 export default function App() {
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    fetchBlogs()
+  }, [])
+
+  async function fetchBlogs() {
+    try {
+      const blogData = await API.graphql(graphqlOperation(listBlogs))
+      const blogs = blogData.data.listBlogs.items
+      setBlogs(blogs)
+    } catch (err) {
+      console.log('error fetching blogs')
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Hello world!</Text>
+      <Text>{JSON.stringify({ blogs }, null, 2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
